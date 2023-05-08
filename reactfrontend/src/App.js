c4be0469679beb9f73bb0e3e16ee4869ad3b8f23
Neelzee <nilsien2001@gmail.com>
2023-05-09 00:00:07 +0200

    Gave it a darkmode look

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -6,7 +6,7 @@ export function Navbar() {
   return (
     <div className="navbar">
       <div className="homeBtn">
-        <a>Home</a>
+        <a href="">Home</a>
       </div>
       <div className="socials">
         <div>
@@ -17,7 +17,7 @@ export function Navbar() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export function Test() {
@@ -27,5 +27,23 @@ export function Test() {
         AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
       </h1>
     </div>
-  )
+  );
+}
+
+
+export function AppTest() {
+  return (
+    <div className="app">
+      <nav>
+        <a href="#">Home</a>
+        <div className="social-links">
+          <a href="https://github.com/Neelzee/">Github</a>
+          <a href="https://www.linkedin.com/in/nils-michael-fitjar-4346a3234/">Linkedin</a>
+        </div>
+      </nav>
+      <div className="hello-world">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
 }
--- reactfrontend/src/index.js
+++ reactfrontend/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {Navbar, Test} from './App';
+import './styles.css';
+import {Navbar, Test, AppTest} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <Test />
+    <AppTest />
   </React.StrictMode>
 );
 
