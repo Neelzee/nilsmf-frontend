8eef38b79201945282440225e277ed98134cbbdd
Neelzee <nilsien2001@gmail.com>
2023-11-06 18:30:06 +0100

    reset

--- djangobackend/csrftoken/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('csrf_token', views.get_csrf_token, name='csrf_token')
-]
--- reactfrontend/src/components/utils/Utils.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Contains the URL to the api-endpoint
- */
-export function ApiRoot(): string {
-    return "http://localhost:8000/api/";
-}
