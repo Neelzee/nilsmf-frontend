49578cb0f63a4c4aa64b881abf3038731608831f
Neelzee <nilsien2001@gmail.com>
2023-11-06 18:53:40 +0100

    added error page

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
 import { Root } from "./routes/root";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ErrorPage } from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
