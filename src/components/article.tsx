0ac53078972ab43656e33b649ca22cfd1d03c328
Neelzee <nilsien2001@gmail.com>
2024-02-11 22:28:50 +0100

    reworked

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -2,15 +2,17 @@ import axios from "axios";
 import { ApiRoot } from "../utils/funcs";
 
 export const getArticle = async (file: string) => {
-  let content = "";
-  await axios
-    .get(ApiRoot(`article/${file}`))
-    .then((res) => {
-      content = res.data;
-      console.log(res);
-    })
-    .catch((err) => console.log(err))
-    .finally(() => (content = "# NO FILE FOUND"));
+	let content = "";
+	await axios
+		.get(ApiRoot(`article/${file}`))
+		.then((res) => {
+			content = res.data;
+			console.log(res);
+		})
+		.catch((err) => console.log(err))
+		.finally(() => {
+			content = "# NO FILE FOUND";
+		});
 
-  return content;
+	return content;
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,12 @@ import "../styles/navbar.scss";
 import { Link } from "react-router-dom";
 
 export function NavBar(): ReactElement {
-  return (
-    <nav>
-      <Link to={"/"}>./~</Link>
-      <Link to={"/projects"}>./~/projects</Link>
-      <Link to={"/about"}>./~/about</Link>
-      <Link to={"/contact"}>./~/contact</Link>
-    </nav>
-  );
+	return (
+		<nav>
+			<Link to={"/"}>~</Link>
+			<Link to={"/projects"}>~/projects</Link>
+			<Link to={"/about"}>~/about</Link>
+			<Link to={"/contact"}>~/contact</Link>
+		</nav>
+	);
 }
